d71dae0acfc1e5b3d9fc06aa4bd63c54ff89b066
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 06:38:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ end
 
 def valid_move?(board, index)
 
-    index.between?(0,8) && position_taken?(board,index) == true
+    index.between?(0,8) && position_taken?(board,index)
 end
 
 def turn(board)
