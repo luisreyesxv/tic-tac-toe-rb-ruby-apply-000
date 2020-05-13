2e4ca0cf03e0f18ae95ce24c6cf27c526ed82ca0
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 06:19:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,3 +15,20 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index] == " "
+end
+
+def valid_move?(board, index)
+
+    index.between?(0,8) && position_taken?(board,index) == true
+end
